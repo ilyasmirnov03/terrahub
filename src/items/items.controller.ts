69284059cf15abea66015108afe48ad92770db3e
Ilya Smirnov <ilua-smirnof@yandex.ru>
2023-01-21 18:35:13 +0100

    Added a few new methods

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,12 +1,19 @@
 import { Controller, Get } from "@nestjs/common";
 import { ItemsService } from "./items.service";
-import { Item } from "./schemas/item.schema";
 
 @Controller("items")
 export class ItemsController {
   constructor(private readonly itemsService: ItemsService) {}
-  @Get("get")
-  getItems() {
-    return this.itemsService.getItems();
+  @Get("all")
+  getAllItems() {
+    return this.itemsService.getAllItems();
+  }
+  @Get("total")
+  getTotalNumber() {
+    return this.itemsService.getTotalNumber();
+  }
+  @Get("random")
+  getRandomItem() {
+    return this.itemsService.getRandomItem();
   }
 }
--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -6,7 +6,15 @@ import { Item, ItemDocument } from "./schemas/item.schema";
 @Injectable()
 export class ItemsService {
   constructor(@InjectModel(Item.name) private itemModel: Model<ItemDocument>) {}
-  getItems() {
+  getAllItems() {
     return this.itemModel.find();
+  }
+  async getTotalNumber() {
+    const result = await this.itemModel.find();
+    return result.length;
+  }
+  async getRandomItem() {
+    const result = await this.itemModel.find();
+    return this.itemModel.findOne({id: Math.ceil(Math.random() * result.length)});
   }
 }
