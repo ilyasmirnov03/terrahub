7b65809d72d9146bb736cbcfbef892233ff089ce
Ilya Smirnov <ilua-smirnof@yandex.ru>
2023-01-28 16:04:11 +0100

    Enabled CORS for 3rd party connections.

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   // launch server on port 3000
   await app.listen(3000);
 }
