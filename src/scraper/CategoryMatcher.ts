41c4fd4eef3658307335eae8ed4c8df6b1f2bf8a
Ilya Smirnov <ilua-smirnof@yandex.ru>
2023-01-28 18:47:37 +0100

    Gives category to an item depending on its name

---
+++ src/scraper/CategoryMatcher.ts
@@ -0,0 +1,13 @@
+export const categoryMatcher: object = {
+  block: "block",
+  brick: "block",
+  wall: "wall",
+  torch: "torch",
+  ore: "ore",
+  dye: "dye",
+  potion: "potion",
+  banner: "banner",
+  trophy: "trophy",
+  relic: "relic",
+  statues: "statues"
+}
