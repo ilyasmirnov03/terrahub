ab40a731960ad4671a68e8d4b027d1b7ffc75b43
Ilya Smirnov <ilua-smirnof@yandex.ru>
2023-01-28 18:51:21 +0100

    Removed HttpModule

--- src/scraper/scraper.module.ts
+++ src/scraper/scraper.module.ts
@@ -1,12 +1,8 @@
 import { Module } from "@nestjs/common";
 import { ScraperController } from "./scraper.controller";
 import { ScraperService } from "./scraper.service";
-import { HttpModule } from "@nestjs/axios";
 
 @Module({
-  imports: [
-    HttpModule
-  ],
   controllers: [ScraperController],
   providers: [ScraperService]
 })
