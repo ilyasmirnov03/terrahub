8300ef4fbfff8f8bd21039293a38ec9d7fb8e0ee
Ilya Smirnov <ilua-smirnof@yandex.ru>
2023-02-06 12:04:02 +0100

    Added category selection

--- src/app/components/items/items-container/items-container.component.ts
+++ src/app/components/items/items-container/items-container.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {Item} from "../../../Item";
 import {ItemsService} from "../../../services/items.service";
 
@@ -13,6 +13,7 @@ export class ItemsContainerComponent implements OnInit{
   ) {}
   items: Item[] = [];
   categories: string[] = [];
+  @ViewChild("visibleItems") numberOfVisibleItemsElement!: ElementRef;
 
   //get items and categories from database
   ngOnInit() {
@@ -23,10 +24,27 @@ export class ItemsContainerComponent implements OnInit{
       this.categories = categories;
     });
   }
+
+  //on click on category event
   selectCategory($event: MouseEvent) {
     let e = $event.target as HTMLSpanElement;
-    if (e.tagName === "SPAN") {
-      e.classList.add("selected");
+    //only spans accepted from now on
+    if (e.tagName !== "SPAN") return;
+    e.classList.toggle("selected");
+    //if no category is selected => make every item appear
+    if (e.closest("div")?.querySelectorAll("span.selected").length === 0) {
+      document.querySelectorAll("app-item.hidden").forEach(item => {item.classList.remove("hidden")});
+    } else {
+      let selector: string = "";
+      //constructing the selector, ex: ".class,.class1,.class2"
+      e.closest("div")?.querySelectorAll("span.selected").forEach(category => {
+        selector += `.${category.textContent},`;
+      });
+      selector = selector.substring(0, selector.length-1);
+      //hide everything that is not hidden
+      document.querySelectorAll(`app-item:not(.hidden)`).forEach(item => {item.classList.add("hidden")});
+      //make selected items appear
+      document.querySelectorAll(`app-item${selector}`).forEach(item => {item.classList.remove("hidden")});
     }
   }
 }
