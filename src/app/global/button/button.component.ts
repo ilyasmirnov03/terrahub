6e24450765afe74d01a9a231a784fe6218e7234c
Ilya Smirnov <ilua-smirnof@yandex.ru>
2023-05-08 23:58:17 +0200

    style(Button): Hover effect
    Hover effect

--- src/app/global/button/button.component.ts
+++ src/app/global/button/button.component.ts
@@ -3,7 +3,6 @@ import {Component, EventEmitter, Output} from '@angular/core';
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
-  styleUrls: ['./button.component.css']
 })
 export class ButtonComponent {
   @Output() btnClick = new EventEmitter;
