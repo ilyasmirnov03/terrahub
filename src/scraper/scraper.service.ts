3c4fbc211762b4095129ac37a0c62724b5c3a280
Ilya Smirnov <ilua-smirnof@yandex.ru>
2023-06-14 01:14:01 +0200

    feat(Entities): Entities categories scraping and misc improvements
    Entities categories scraping and misc improvements

--- src/scraper/scraper.service.ts
+++ src/scraper/scraper.service.ts
@@ -6,8 +6,33 @@ import { Npc } from "../interfaces/NPC";
 
 @Injectable()
 export class ScraperService {
-  async getItemsFromWiki() {
-    const browser = await puppeteer.launch();
+  private async getCategory(page, link): Promise<{
+    version: string | null,
+    category: any
+  }> {
+    try {
+      await page.goto(link);
+    } catch (err) {
+      console.log(err);
+      console.log(link);
+      return {
+        version: null,
+        category: null
+      };
+    }
+    return page.evaluate(() => {
+      const versionName = Array.from(document.querySelectorAll("a[title^=\"Desktop\"].mw-redirect")).pop()?.textContent || null;
+      const version = versionName === "Desktop-Release" ? "1.0" : versionName?.split(" ")[1] || null;
+      return {
+        version: version,
+        category: document.querySelectorAll(".tag")[0]?.firstElementChild?.textContent ||
+          document.querySelectorAll(".tag")[0]?.textContent || null
+      };
+    });
+  }
+
+  public async getItemsFromWiki() {
+    const browser = await puppeteer.launch({ headless: "new" });
 
     const page = await browser.newPage();
 
@@ -15,30 +40,53 @@ export class ScraperService {
 
     const result = await page.evaluate((categoryMatcher) => {
       const array = [];
-      document.querySelectorAll("table:nth-child(2)>tbody>tr").forEach((item) => {
+      document.querySelectorAll("table:nth-child(2)>tbody>tr").forEach(async (item) => {
         const id = item.firstElementChild.textContent;
         const name = item.children[1].textContent.toLowerCase();
         const internalName = item.lastElementChild.textContent;
-        const link = "https://terraria.wiki.gg" + item.children[1].firstElementChild.getAttribute("href");
-        let category: { group: string, name: string };
+        const link = item.children[1].firstElementChild.getAttribute("href");
+        const completeLink = link ? `https://terraria.wiki.gg${link}` : null;
 
-        let words = name.split(" ");
-        category = (categoryMatcher[words[words.length - 1]]) ? categoryMatcher[words[words.length - 1]] : "";
+        const words = name.split(" ");
+        const category: {
+          group: string,
+          name: string
+        } = categoryMatcher[words[words.length - 1]] ? categoryMatcher[words[words.length - 1]] : {group: '', name: ''};
 
         // build the object using Item interface
-        const itemToPush: Item = { id: id, name: name, internalName: internalName, category: category, link: link };
+        const itemToPush: Item = {
+          id: id,
+          name: name,
+          internalName: internalName,
+          category,
+          version: "",
+          link: completeLink
+        };
         array.push(itemToPush);
       });
       return array;
     }, categoryMatcher);
+
+    for (const item of result) {
+      if (item.link !== null) {
+        const result = await this.getCategory(page, item.link);
+        if (item.category.name == '') {
+          item.category = {
+            group: "",
+            name: result.category
+          };
+        }
+        item.version = result.version;
+      }
+    }
 
     await browser.close();
 
     return result;
   }
 
-  async getNPCsFromWiki() {
-    const browser = await puppeteer.launch();
+  public async getNPCsFromWiki() {
+    const browser = await puppeteer.launch({ headless: "new" });
 
     const page = await browser.newPage();
 
@@ -47,18 +95,40 @@ export class ScraperService {
     const result = await page.evaluate(() => {
       const array = [];
       document.querySelectorAll(".terraria>tbody>tr").forEach((npc) => {
-        const id = npc.firstElementChild?.textContent.trim() || 'undefined';
-        const name = npc.children[1].firstElementChild.firstElementChild.firstElementChild?.textContent.toLowerCase() || 'undefined';
-        const internalName = npc.lastElementChild.firstElementChild?.textContent || 'undefined';
+        const id = npc.firstElementChild?.textContent.trim() || "undefined";
+        const name = npc.children[1].firstElementChild.firstElementChild.firstElementChild?.textContent.toLowerCase() || "undefined";
+        const internalName = npc.lastElementChild.firstElementChild?.textContent || "undefined";
         const image = `https://terraria.wiki.gg${npc.children[2].firstElementChild.firstElementChild.getAttribute("src")}`;
         const link = `https://terraria.wiki.gg${npc.children[1].firstElementChild.firstElementChild.firstElementChild.getAttribute("href")}`;
 
         // build the object using Npc interface
-        const npcToPush: Npc = { id: id, name: name, internalName: internalName, image: image, link: link };
+        const npcToPush: Npc = {
+          id,
+          name,
+          internalName,
+          image,
+          link,
+          version: "",
+          category: {
+            group: "",
+            name: ""
+          }
+        };
         array.push(npcToPush);
       });
       return array;
     });
+
+    for (const npc of result) {
+      if (npc.link !== null) {
+        const result = await this.getCategory(page, npc.link);
+        npc.category = {
+          group: "",
+          name: result.category
+        };
+        npc.version = result.version;
+      }
+    }
 
     await browser.close();
 
