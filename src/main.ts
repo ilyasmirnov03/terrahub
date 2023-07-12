1ab744ac85b5630430ccd7e2b9c38cf857858198
leeass <ilua-smirnof@yandex.ru>
2023-07-12 23:58:48 +0200

    fix(App): Listen only to env port
    Listen only to env port

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.enableCors();
-  await app.listen(process.env.PORT || 8100);
+  await app.listen(process.env.PORT);
 }
 bootstrap();
