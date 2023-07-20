983618437dcfe9d11108d0318f081cebc37c943b
leeass <ilua-smirnof@yandex.ru>
2023-07-21 00:16:28 +0200

    feat(Header): Menu is hidden on mobile
    Menu is hidden on mobile

--- src/app/global/header/header.component.ts
+++ src/app/global/header/header.component.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  @ViewChild('menu')
+  private menu!: ElementRef;
 
+  public isOpened = false;
+
+  public handleMenu() {
+    this.menu.nativeElement.classList.toggle('translate-x-full');
+    this.isOpened = !this.isOpened;
+  }
 }
