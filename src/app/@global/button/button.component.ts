d03fd1039a8abbbd521ad80ab83200fc883a7219
leeass <ilua-smirnof@yandex.ru>
2023-07-23 12:35:14 +0200

    feat(Button): Added reusable button component
    Added reusable button component

---
+++ src/app/@global/button/button.component.ts
@@ -0,0 +1,14 @@
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+})
+export class ButtonComponent {
+  @Output()
+  public btnClick = new EventEmitter;
+
+  public onClick(event: Event) {
+    this.btnClick.emit(event);
+  }
+}
