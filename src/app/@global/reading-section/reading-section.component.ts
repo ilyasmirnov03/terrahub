2eaa4785abb59294bb00fabfadb43f8682303d35
leeass <ilua-smirnof@yandex.ru>
2023-08-06 12:38:31 +0200

    feat(App): Added a reading-section component to use for word based content
    Added a reading-section component to use for word based content

---
+++ src/app/@global/reading-section/reading-section.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reading-section',
+  templateUrl: './reading-section.component.html'
+})
+export class ReadingSectionComponent {
+
+}
