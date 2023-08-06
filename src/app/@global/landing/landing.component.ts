2c8c7b6b59e2c4a46be60998a3e227ee655dac0f
leeass <ilua-smirnof@yandex.ru>
2023-08-06 15:36:51 +0200

    feat(Landing): Show app version
    Show app version

--- src/app/@global/landing/landing.component.ts
+++ src/app/@global/landing/landing.component.ts
@@ -1,15 +1,27 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Meta} from "@angular/platform-browser";
 import {routes} from "../../constants/routes.const";
+import {HttpClient} from "@angular/common/http";
+import {environment} from "../../../environments/environment";
 
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html'
 })
-export class LandingComponent {
-  constructor(private readonly meta: Meta) {
+export class LandingComponent implements OnInit {
+  public version: string = '';
+  public routes = routes;
+
+  constructor(
+    private readonly meta: Meta,
+    private readonly http: HttpClient,
+  ) {
     this.meta.updateTag({name: 'description', content: 'The place to find all terraria items and entities'});
   }
 
-  public routes = routes;
+  ngOnInit() {
+    this.http.get(`${environment.apiUrl}/version`, {responseType: 'text'}).subscribe((versionNumber: string) => {
+      this.version = versionNumber;
+    });
+  }
 }
