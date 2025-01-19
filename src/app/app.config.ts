883772d588f86bc6fc0940b598ca7c0edac7bfbf
Ilya Smirnov <ilua.smirnof@gmail.com>
2025-01-19 12:42:05 +0100

    feat(matomo): Use matomo
    Use matomo to track app usage

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,10 +2,20 @@ import {ApplicationConfig} from "@angular/core";
 import {provideRouter} from "@angular/router";
 import {routes} from "./app.routes";
 import {provideHttpClient, withInterceptorsFromDi} from "@angular/common/http";
+import {provideMatomo, withRouter} from "ngx-matomo-client";
+import {environment} from "../environments/environment";
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(withInterceptorsFromDi()),
+    provideMatomo(
+      {
+        siteId: 1,
+        trackerUrl: "https://stats.ilyasmirnov.dev",
+        disabled: !environment.production,
+      },
+      withRouter(),
+    )
   ],
 };
