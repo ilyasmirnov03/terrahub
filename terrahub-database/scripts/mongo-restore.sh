4744e4453ca12c99b67b48f6646fecfb57533451
ilyasmirnov03 <ilua.smirnof@gmail.com>
2025-08-04 19:01:17 +0200

    feat(database): Update db restore script
    Update db restore script

--- terrahub-database/scripts/mongo-restore.sh
+++ terrahub-database/scripts/mongo-restore.sh
@@ -1,1 +1,1 @@
-mongorestore --db terrahub /backup
+mongorestore --db terrahub --password ${MONGO_INITDB_ROOT_PASSWORD} --username ${MONGO_INITDB_ROOT_USERNAME} --uri mongodb://localhost:27018 /backup
